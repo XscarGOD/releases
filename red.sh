d1303828f3c49b35c402e71dc1a4568efbebb4a7
XscarGOD <107100303+XscarGOD@users.noreply.github.com>
2023-05-27 22:51:28 +0530

    Create red.sh

---
+++ red.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash 
+
+git clone https://github.com/lineage-redwood/android_device_xiaomi_redwood device/xiaomi/redwood
+git clone https://github.com/lineage-redwood/android_device_xiaomi_sm8350-common device/xiaomi/sm8350-common
+git clone https://github.com/lineage-redwood/android_vendor_xiaomi_redwood vendor/xiaomi/redwood
+git clone https://github.com/lineage-redwood/android_vendor_xiaomi_sm8350-common vendor/xiaomi/sm8350-common
+git clone https://github.com/lineage-redwood/android_kernel_xiaomi_sm8350 kernel/xiaomi/sm8350
